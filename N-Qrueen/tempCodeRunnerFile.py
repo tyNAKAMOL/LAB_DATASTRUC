1085a99a6f9ba602688a7bba5a19290e251fb284
tyNAKAMOL <beauty.nkm39@gmail.com>
2021-10-10 22:45:24 +0700

    n-queen

---
+++ N-Qrueen/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+ # __ = 0
+        # while __ < self.N:
+        #     for _ in range(self.N):
+        #         print(s[_][__])
+        #         x.append(s[_][__])
+        #     __ += 1
+        #         # s.append(sum(s[y]))
